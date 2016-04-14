e84e4676eb8b9593a564a2ac15fe582ec64aba0d
Ryan Mottley <ligerzero459@gmail.com>
2016-04-13 21:51:53 -0500

    load json API and read settings from settings.json

--- smeltery_controller.lua
+++ smeltery_controller.lua
@@ -5,8 +5,21 @@
   Date: 13 April 2016
 ]]
 
+-- DEPENDENCIES
+os.loadAPI("json")
+
+-- LOADED SETTINGS
+local settings_json = fs.open("settings.json")
+local settings = json.decode(settings_json:read("*all"))
+
+local smeltery_side = settings.smeltery
+local redstone_size = settings.redstone
+
+-- LOCAL VARIABLES
 local smeltery
 
 function getSmeltery()
-  smeltery = peripheral.wrap()
+  smeltery = peripheral.wrap(smeltery_side)
 end
+
+function 
