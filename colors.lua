df7159c69f252983515e8c117970c2e6922b0d5d
Ryan Mottley <ligerzero459@gmail.com>
2016-04-15 23:12:11 -0500

    use proper string library for lowercase

--- colors.lua
+++ colors.lua
@@ -1,36 +1,36 @@
 function getColor(color)
 
-  if strlower(color) == "black" then
+  if string.lower(color) == "black" then
     return colors.black
-  elseif strlower(color) == "red" then
+  elseif string.lower(color) == "red" then
     return colors.red
-  elseif strlower(color) == "green" then
+  elseif string.lower(color) == "green" then
     return colors.green
-  elseif strlower(color) == "brown" then
+  elseif string.lower(color) == "brown" then
     return colors.brown
-  elseif strlower(color) == "blue" then
+  elseif string.lower(color) == "blue" then
     return colors.blue
-  elseif strlower(color) == "purple" then
+  elseif string.lower(color) == "purple" then
     return colors.purple
-  elseif strlower(color) == "cyan" then
+  elseif string.lower(color) == "cyan" then
     return colors.cyan
-  elseif strlower(color) == "light gray" then
+  elseif string.lower(color) == "light gray" then
     return colors.lightGray
-  elseif strlower(color) == "gray" then
+  elseif string.lower(color) == "gray" then
     return colors.gray
-  elseif strlower(color) == "pink" then
+  elseif string.lower(color) == "pink" then
     return colors.pink
-  elseif strlower(color) == "lime" then
+  elseif string.lower(color) == "lime" then
     return colors.lime
-  elseif strlower(color) == "yellow" then
+  elseif string.lower(color) == "yellow" then
     return colors.yellow
-  elseif strlower(color) == "light blue" then
+  elseif string.lower(color) == "light blue" then
     return colors.lightBlue
-  elseif strlower(color) == "magenta" then
+  elseif string.lower(color) == "magenta" then
     return colors.magenta
-  elseif strlower(color) == "orange" then
+  elseif string.lower(color) == "orange" then
     return colors.orange
-  elseif strlower(color) == "white" then
+  elseif string.lower(color) == "white" then
     return colors.white
   end
 
